e09de4aacbef023766758ecc032a62cb85976b00
barbar17 <akbarmaulana644@gmail.com>
2021-12-28 21:07:41 +0700

    Update sketch_dec20a.ino

--- sketch_dec20a.ino
+++ sketch_dec20a.ino
@@ -1,19 +1,29 @@
 #include "DHT.h"
+#include <BlynkSimpleEsp32.h>
 
+#define BLYNK_PRINT Serial
 #define sensorSuhu 5
 #define kipas 23
 #define tipeSensor DHT11
 #define sensorPintu 4
 #define led 2
 #define buzz 18
+#define alarm 21
+
+char auth[]="KWt6YDepcUpaQnigGsX9GT2bC_hHV49s";
+char ssid[]="MSI_173";
+char pass[]="123456789";
 
 DHT dht(sensorSuhu, tipeSensor);
 
 bool state;
+bool mod;
 String doorState;
 
 void setup() {
   Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(alarm, INPUT);
 
   //Door Sensor
   pinMode(sensorPintu, INPUT_PULLUP);
@@ -28,14 +38,18 @@ void setup() {
 }
 
 void loop() {
+  Blynk.run();
   //Door Sensor
   state = digitalRead(sensorPintu);
-  if(state) {
-    doorState = "DOOR_CLOSED";
-    digitalWrite(buzz, LOW);
-  } else {
-    doorState = "DOOR_OPEN";
-    digitalWrite(buzz, HIGH);
+  mod = digitalRead(alarm);
+  if(mod == LOW){
+    if(state) {
+      doorState = "DOOR_CLOSED";
+      digitalWrite(buzz, LOW);
+    } else {
+      doorState = "DOOR_OPEN";
+      digitalWrite(buzz, HIGH);
+    }
   }
   digitalWrite(led, !state);
   Serial.println(doorState);
