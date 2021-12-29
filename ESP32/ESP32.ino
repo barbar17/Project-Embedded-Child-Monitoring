5ec85f0307b1a7891d4d85dcf720260b1a895f1c
barbar17 <akbarmaulana644@gmail.com>
2021-12-29 15:52:03 +0700

    Update ESP32.ino

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,72 +1,107 @@
 #include "DHT.h"
 #include <BlynkSimpleEsp32.h>
 
-#define BLYNK_PRINT Serial
+//ESP32 PIN
 #define sensorSuhu 5
 #define kipas 23
-#define tipeSensor DHT11
 #define sensorPintu 4
 #define led 2
 #define buzz 18
 #define alarm 21
 
+//Blynk Virtual PIN
+#define blynk_tmpt V1
+#define lcd_pin V2
+
+//Inisiasi LCD pada Blynk
+WidgetLCD lcd(lcd_pin);
+
+//Kondifurasi Token Blynk, dan Jaringan WiFi
 char auth[]="KWt6YDepcUpaQnigGsX9GT2bC_hHV49s";
 char ssid[]="MSI_173";
 char pass[]="123456789";
 
+//Konfigurasi Sensor DHT11
+#define tipeSensor DHT11
 DHT dht(sensorSuhu, tipeSensor);
 
 bool state;
 bool alert;
+bool stateChange;
 String doorState;
 
 void setup() {
   Serial.begin(115200);
+  
+  //Inisiasi Blynk
   Blynk.begin(auth, ssid, pass);
+  
+  //Mode Alarm
   pinMode(alarm, INPUT);
 
   //Door Sensor
   pinMode(sensorPintu, INPUT_PULLUP);
+  stateChange = 0;
+
+  //LED pada ESP32
   pinMode(led, OUTPUT);
+
+  //Buzzer
   pinMode(buzz, OUTPUT);
 
   //Kipas
   pinMode(kipas,OUTPUT);
   digitalWrite(kipas,1);
-  Serial.println(F("Percobaan menggunakan sensor suhu dan kelembapan!"));
+
+  //Inisiasi DHT11
   dht.begin();
 }
 
 void loop() {
+  //Menjalankan Blynk
   Blynk.run();
-  //Door Sensor
+  
+  //Membaca Kondisi Pintu
   state = digitalRead(sensorPintu);
+  
+  //Membaca Mode Alarm
   alert = digitalRead(alarm);
+
+  //Membaca Perubahan Kondisi Pintu
+  if(stateChange != state) {
+    lcd.clear();
+    stateChange = state;
+  }
+
+  //Membaca Kondisi Pintu
+  if(state) {
+      doorState = "Pintu Tertutup";
+    } else {
+      doorState = "Pintu Terbuka";
+    }
+
+  //Print Kondisi Pintu Pada LCD Blynk
+  lcd.print(0, 0, doorState);
+
+  //Kondisi Alarm Saat Menyala
   if(alert == HIGH){
     if(state) {
-      doorState = "DOOR_CLOSED";
       digitalWrite(buzz, LOW);
     } else {
-      doorState = "DOOR_OPEN";
       digitalWrite(buzz, HIGH);
     }
   }
-  digitalWrite(led, !state);
-  Serial.println(doorState);
 
-  //Kipas
+  //Menyalakan LED Pada ESP32
+  digitalWrite(led, !state);
+  
+  //Membaca Temperatur Ruangan
   float suhuC = dht.readTemperature();
 
-  if(isnan(suhuC)){
-    Serial.print(F("Sensor DHT11 tidak terbaca!"));
-    return;
-  }
-  
-  Serial.print(F("% Suhu : "));
-  Serial.print(suhuC);
-  Serial.println(F(" C "));
-  Serial.println("_________________");
+  //Menampilkan Suhu Pada Blynk
+  Blynk.virtualWrite(blynk_tmpt, suhuC);
 
+  //Kondisi Kipas Menyala
   if (suhuC>=28){
     digitalWrite(kipas, 0);
   }else if (suhuC<= 25) {
